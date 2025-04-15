0c442822600296850ff8993f0f622a540a53e846
VickyUA <vickyy379@gmail.com>
2025-04-15 23:33:55 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+// Описаний в документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
 
-console.log(galleryItems);
+const newGallery = document.querySelector('.gallery');
+
+const markUp = arr => {
+  return arr
+    .map(
+      item =>
+        `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>
+</li>`
+    )
+    .join('');
+};
+
+newGallery.insertAdjacentHTML('beforeend', markUp(galleryItems));
+
+let picture = new SimpleLightbox('.gallery__item a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
