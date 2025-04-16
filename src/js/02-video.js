ac22e6c16c0a370af902588df4cae77559cfb58a
VickyUA <vickyy379@gmail.com>
2025-04-16 15:46:52 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+// const player = new Player('handstick', {
+//     id: 19231868,
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
